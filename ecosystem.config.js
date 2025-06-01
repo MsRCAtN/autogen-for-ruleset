33418213c7c08055384921c04f8cd21b838f45e7
MsRCAtN <sa46l112@hotmail.com>
2025-06-01 14:09:01 +0800

    add PM2 ecosystem config for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+      name   : "autogen",                     
+      script : "./server/index.js",            
+      cwd    : "/root/autogen-for-ruleset/",  
+      watch  : false,                         
+      env    : {
+      }
+    }]
+  };
